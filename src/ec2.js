20afbd1f973d244230a9b078f1f64a9054f4d87f
tedere93 <tedere87@gmail.com>
2017-03-31 21:41:02 +0300

    Split functions.
    Codul jS a fost impartit in functii separate pentru o mai usoara
    folosire(update-ul acesta s-a facut in versiunea trecuta), si s-a
    finalizat solverul astfel incat sa poata returna solutii complexe.

--- src/ec2.js
+++ src/ec2.js
@@ -9,8 +9,8 @@ function read(){
 }
 
 function solveDelta(coef){
-	var deltaa=coef[1]*coef[1] - 4*coef[0]*coef[2];
-	return deltaa;
+	var delta=coef[1]*coef[1] - 4*coef[0]*coef[2];
+	return delta;
 
 }
 
@@ -24,35 +24,34 @@ function solveRoots(coef,delta){
 		x1=(-coef[1])/2*coef[0];
 		x1im=Math.sqrt(-delta)/2*coef[0];
 		x2=(-coef[1])/2*coef[0];
-		x2im=-Math.sqrt(-delta)/2*coef[0];
-		// document.getElementById("x1").innerHTML =x1.toString()+"+"+x1im.toString()+"i";
-		// document.getElementById("x2").innerHTML =x2.toString()+x2im.toString()+"i";	
+		x2im=-Math.sqrt(-delta)/2*coef[0];	
 	}
 	else if(delta>0){
 		x1 = ((-coef[1])+Math.sqrt(delta))/(2*coef[0]);
 		x2 = ((-coef[1])-Math.sqrt(delta))/(2*coef[0]);
-		// document.getElementById("x1").innerHTML = "x1 =" + x1;
-		// document.getElementById("x2").innerHTML = "x2 =" + x2;
 	}
 	var radacini=[x1,x2,x1im,x2im];
 	return radacini;
 }
 
 function showAnswer(radacini){
-
-	document.getElementById("x1").innerHTML = "x1 =" + radacini[0] + "+" + radacini[2] + "i";
-	document.getElementById("x2").innerHTML = "x2 =" + radacini[1] + radacini[3] + "i";
-}
+	if(radacini[2]||radacini[3]!=0){
+		document.getElementById("x1").innerHTML = "x1 =" + radacini[0] + "+" + radacini[2] + "i";
+		document.getElementById("x2").innerHTML = "x2 =" + radacini[1] + radacini[3] + "i";
+	}
+	else{
+		document.getElementById("x1").innerHTML = "x1 =" + radacini[0];
+		document.getElementById("x2").innerHTML = "x2 =" + radacini[1];
+	} 
+	}
 
 function solve(){
-			x1=x2=x1im=x2im=0;
+			// x1=x2=x1im=x2im=0;
 			var coef = read();
-			var deltaa = solveDelta(coef);
-			var radacini=solveRoots(coef,deltaa);
-			
-			// document.getElementById("delta").innerHTML = "Delta=" + delta;
+			var delta = solveDelta(coef);
+			var radacini=solveRoots(coef,delta);
 			showAnswer(radacini);
-			document.getElementById("delta").innerHTML = "Delta=" + deltaa;
+			document.getElementById("delta").innerHTML = "Delta=" + delta;
 			document.getElementById("aa").innerHTML = "a= " + coef[0];
 			document.getElementById("bb").innerHTML = "b= " + coef[1];
 			document.getElementById("cc").innerHTML = "c= " + coef[2];
