a9562c1dd945b85ddc860fbc92cc48495e855d99
tedere93 <tedere87@gmail.com>
2017-04-07 07:47:34 +0300

    Desenare cerc

--- src/senzori/senzori.js
+++ src/senzori/senzori.js
@@ -9,4 +9,16 @@ function ondeviceorientation(evt){
 	document.getElementById("b").innerHTML = "Beta =" + beta;
 	document.getElementById("c").innerHTML = "Gamma =" +gamma;
 
+
+	var canvas = document.getElementById("cnv");
+	var context = canvas.getContext("2d");
+
+	var raza = 50;
+	var centru = {x:canvas.width/2, y:canvas.height/2};
+
+	context.clearRect(0,0,canvas.width,canvas.height);
+	context.beginPath();
+	context.arc(centru.x+gamma*(canvas.width/2 - raza)/90,centru.y+beta*(canvas.height/2-raza)/90,raza,0,2*Math.PI);
+	context.stroke();
+
 }
