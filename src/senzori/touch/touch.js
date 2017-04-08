401bebc7c322f04ff8ed2ee91b30893db990deb5
tedere93 <tedere87@gmail.com>
2017-04-08 08:00:35 +0300

    Adaugare culori multiple

--- src/senzori/touch/touch.js
+++ src/senzori/touch/touch.js
@@ -1,13 +1,17 @@
 window.addEventListener("touchstart",ontouchstart);
 
+var touch_colors = ["red","green","blue","pink"];
+
 function ontouchstart(event){
 	var touches = event.changedTouches;
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
 
-	for(var i = 0; i < touches.lenght;i++){
+	for(var i = 0; i < touches.length;i++){
 		ctx.beginPath();
-		ctx.arc(touches[i].pageX, touches[i].pageY,5,0,0);
+		ctx.strokeStyle=touch_colors[i];
+		ctx.arc(touches[i].pageX, touches[i].pageY,40,0,2*Math.PI);
+		
 		ctx.stroke();
 	}
 }
